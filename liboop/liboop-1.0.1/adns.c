9aca8342e463f1cd2a3737a25119abeb716e0139
arvtom <tomkusarvydas@gmail.com>
2025-09-10 18:13:05 +0000

    add liboop source tag 1.0.1

---
+++ liboop/liboop-1.0.1/adns.c
@@ -0,0 +1,178 @@
+/* adns.c, liboop, copyright 1999 Dan Egnor
+
+   This is free software; you can redistribute it and/or modify it under the
+   terms of the GNU Lesser General Public License, version 2.1 or later.
+   See the file COPYING for details. */
+
+#ifdef HAVE_ADNS
+
+#include "oop.h"
+#include "adns.h"
+#include "oop-adns.h"
+
+#include <assert.h>
+
+#ifdef HAVE_STRING_H
+#include <string.h>   /* Needed on NetBSD1.1/SPARC due to bzero/FD_ZERO. */
+#endif
+
+#ifdef HAVE_STRINGS_H
+#include <strings.h>  /* Needed on AIX 4.2 due to bzero/FD_ZERO. */
+#endif
+
+struct oop_adapter_adns {
+	oop_source *source;
+	oop_adapter_select *select;
+	adns_state state;
+	int count;
+};
+
+struct oop_adns_query {
+	oop_adapter_adns *a;
+	adns_query query;
+	oop_adns_call *call;
+	void *data;
+};
+
+static oop_call_select on_select;
+static oop_call_time on_process;
+static void set_select(oop_adapter_adns *);
+
+oop_adapter_adns *oop_adns_new(
+	oop_source *source,
+	adns_initflags flags,FILE *diag) 
+{
+	oop_adapter_adns *a = oop_malloc(sizeof(*a));
+	if (NULL == a) return NULL;
+	a->select = NULL;
+	a->state = NULL;
+
+	if (adns_init(&a->state,flags | adns_if_noautosys,diag)
+	|| (NULL == (a->select = oop_select_new(source,on_select,a)))) {
+		if (NULL != a->state) adns_finish(a->state);
+		if (NULL != a->select) oop_select_delete(a->select);
+		oop_free(a);
+		return NULL;
+	}
+
+	a->source = source;
+	a->count = 0;
+	return a;
+}
+
+void oop_adns_delete(oop_adapter_adns *a) {
+	assert(0 == a->count && 
+	       "deleting oop_adapter_adns with outstanding queries");
+	a->source->cancel_time(a->source,OOP_TIME_NOW,on_process,a);
+	oop_select_delete(a->select);
+	adns_finish(a->state);
+	oop_free(a);
+}
+
+oop_adns_query *oop_adns_submit(
+	oop_adapter_adns *a,int *errcode,
+	const char *owner,adns_rrtype type,adns_queryflags flags,
+	oop_adns_call *call,void *data)
+{
+	oop_adns_query *q = oop_malloc(sizeof(*q));
+	int err;
+	if (NULL == q) return NULL;
+
+	err = adns_submit(a->state,owner,type,flags,q,&q->query);
+	if (errcode) *errcode = err;
+	if (err) {
+		oop_free(q);
+		return NULL;
+	}
+
+	q->a = a;
+	q->call = call;
+	q->data = data;
+	++q->a->count;
+	set_select(a);
+	return q;
+}
+
+oop_adns_query *oop_adns_submit_reverse(
+	oop_adapter_adns *a,int *errcode,
+	const struct sockaddr *addr,adns_rrtype type,adns_queryflags flags,
+	oop_adns_call *call,void *data)
+{
+	oop_adns_query *q = oop_malloc(sizeof(*q));
+	int err;
+	if (NULL == q) return NULL;
+
+	err = adns_submit_reverse(a->state,addr,type,flags,q,&q->query);
+	if (errcode) *errcode = err;
+	if (err) {
+		oop_free(q);
+		return NULL;
+	}
+
+	q->a = a;
+	q->call = call;
+	q->data = data;
+	++q->a->count;
+	set_select(a);
+	return q;
+}
+
+void oop_adns_cancel(oop_adns_query *q) {
+	adns_cancel(q->query);
+	--q->a->count;
+	set_select(q->a);
+	oop_free(q);
+}
+
+static void set_select(oop_adapter_adns *a) {
+	fd_set rfd,wfd,xfd;
+	struct timeval buf,*out = NULL,now;
+	int maxfd = 0;
+	FD_ZERO(&rfd);
+	FD_ZERO(&wfd);
+	FD_ZERO(&xfd);
+	gettimeofday(&now,NULL);
+	adns_beforeselect(a->state,&maxfd,&rfd,&wfd,&xfd,&out,&buf,&now);
+	oop_select_set(a->select,maxfd,&rfd,&wfd,&xfd,out);
+}
+
+static void *on_process(oop_source *source,struct timeval when,void *data) {
+	oop_adapter_adns *a = (oop_adapter_adns *) data;
+	adns_answer *r;
+	adns_query query;
+	oop_adns_query *q = NULL;
+	void *adns_data;
+
+	query = NULL;
+	if (0 == adns_check(a->state,&query,&r,&adns_data)) {
+		q = (oop_adns_query *) adns_data;
+		assert(query == q->query);
+	}
+
+	set_select(a);
+
+	if (NULL != q) {
+		oop_adns_call *call = q->call;
+		void *data = q->data;
+		assert(a == q->a);
+		--q->a->count;
+		oop_free(q);
+		source->on_time(source,when,on_process,a);
+		return call(a,r,data);
+	}
+
+	return OOP_CONTINUE;
+}
+
+static void *on_select(
+	oop_adapter_select *select,
+	int num,fd_set *rfd,fd_set *wfd,fd_set *xfd,
+	struct timeval now,void *data)
+{
+	oop_adapter_adns *a = (oop_adapter_adns *) data;
+
+	adns_afterselect(a->state,num,rfd,wfd,xfd,&now);
+	return on_process(a->source,OOP_TIME_NOW,a);
+}
+
+#endif
---
+++ liboop/liboop-1.0.1/glib.c
@@ -0,0 +1,140 @@
+/* glib.c, liboop, copyright 1999 Dan Egnor
+
+   This is free software; you can redistribute it and/or modify it under the
+   terms of the GNU Lesser General Public License, version 2.1 or later.
+   See the file COPYING for details. */
+
+#include "glib.h"
+#include "oop-glib.h"
+#include "oop.h"
+
+#include <assert.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+#ifdef HAVE_POLL_H
+#include <poll.h>
+#endif
+
+#ifdef HAVE_STRING_H
+#  include <string.h>	/* Needed on NetBSD1.1/SPARC due to bzero/FD_ZERO. */
+#endif
+
+#ifdef HAVE_STRINGS_H
+#  include <strings.h>  /* Needed on AIX 4.2 due to bzero/FD_ZERO. */
+#endif
+
+static int use_count = 0;
+static oop_source_sys *sys;
+static oop_adapter_select *sel;
+
+static fd_set read_set,write_set,except_set;
+static int count;
+static void *ret = NULL;
+
+#if GLIB_MAJOR_VERSION >= 2
+static GPollFunc save_poll_func = NULL;
+#endif
+
+static void *on_select(
+	oop_adapter_select *s,int num,fd_set *r,fd_set *w,fd_set *x,
+	struct timeval now,void *unused) 
+{
+	read_set = *r;
+	write_set = *w;
+	except_set = *x;
+	count = num;
+	return &use_count;
+}
+
+static gint on_poll(GPollFD *array,guint num,gint timeout) {
+	struct timeval tv;
+	int i;
+
+	FD_ZERO(&read_set);
+	FD_ZERO(&write_set);
+	FD_ZERO(&except_set);
+	count = 0;
+	for (i = 0; i < num; ++i) {
+		if (array[i].events & G_IO_IN)
+			FD_SET(array[i].fd,&read_set);
+		if (array[i].events & G_IO_OUT)
+			FD_SET(array[i].fd,&write_set);
+		if (array[i].events & G_IO_PRI)
+			FD_SET(array[i].fd,&except_set);
+		/* {G_IO_,POLL}{ERR,HUP,INVAL} don't correspond to anything
+		   in select(2), and aren't `normal' events anyway. */
+		if (array[i].fd >= count)
+			count = 1 + array[i].fd;
+	}
+
+	tv.tv_sec = timeout / 1000;
+	tv.tv_usec = timeout % 1000;
+
+	oop_select_set(sel,count,&read_set,&write_set,&except_set,
+		       timeout < 0 ? NULL : &tv);
+	ret = oop_sys_run(sys);
+
+	if (&use_count != ret) {
+		/* I really wish I could: g_main_quit(glib_loop); */
+		return -1;
+		/* but they even ignore the error return... sigh. */
+	}
+
+	for (i = 0; i < num; ++i) {
+		if (FD_ISSET(array[i].fd,&read_set))
+			array[i].revents |= G_IO_IN;
+		if (FD_ISSET(array[i].fd,&write_set))
+			array[i].revents |= G_IO_OUT;
+		if (FD_ISSET(array[i].fd,&except_set))
+			array[i].revents |= G_IO_PRI;
+	}
+
+	return count;
+}
+
+oop_source *oop_glib_new(void) {
+	if (use_count++) return oop_sys_source(sys);
+
+	sys = oop_sys_new();
+	sel = oop_select_new(oop_sys_source(sys),on_select,NULL);
+
+#if GLIB_MAJOR_VERSION >= 2
+	save_poll_func = g_main_context_get_poll_func(g_main_context_default());
+        g_main_context_set_poll_func(g_main_context_default(), on_poll);
+#else
+        g_main_set_poll_func(on_poll);
+#endif
+
+	return oop_sys_source(sys);
+}
+
+void *oop_glib_return(void) {
+	if (&use_count == ret) return NULL;
+	return ret;
+}
+
+#ifdef HAVE_POLL_H
+static gint real_poll(GPollFD *array,guint num,gint timeout) {
+	assert(sizeof(GPollFD) == sizeof(struct pollfd));
+	return poll((struct pollfd *) array,num,timeout);
+}
+#endif
+
+void oop_glib_delete(void) {
+	assert(use_count > 0 && "oop_glib_delete() called too much");
+	if (0 != --use_count) return;
+
+#if GLIB_MAJOR_VERSION >= 2
+	g_main_context_set_poll_func(g_main_context_default(), save_poll_func);
+#elif defined(HAVE_POLL_H)
+	g_main_set_poll_func(real_poll);
+#else
+	return;
+#endif
+
+	oop_select_delete(sel);
+	oop_sys_delete(sys);
+}
